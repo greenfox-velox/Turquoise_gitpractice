9c317c200ed2805bf50240532496b8dc6f0a1d45
agyenes <aron.gyenes@gmail.com>
2016-08-01 16:53:34 +0200

    c1

---
+++ cheer.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let cheer = (function() {
+  console.log('turkisz!');
+})();
--- greet.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-let greet = function() {
-  console.log('Földváry Péter');
-};
-
-greet();
